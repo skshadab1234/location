1b0cf5bfa17ea4a93a556216c361aaafb3a937da
skshadab1234 <ks615044@gmail.com>
2023-05-28 14:22:25 +0530

    initial commit - 8

--- server/router/auth.js
+++ server/router/auth.js
@@ -4,25 +4,29 @@ const Location = require('../models/Loaction')
 
 
 router.get('/loc', async (req, res) => {
-    const { latitude, longitude } = req.query;
-    const url = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
-    console.log(latitude, longitude);
-    
-    // try {
-    //     const newLoc = new Location({ latitude, longitude, url, status: 'yes' })
-
-    //     newLoc.save((error) => {
-    //         if (error) {
-    //             console.log(error);
-    //         } else {
-    //             console.log('Location saved successfully!');
-    //             res.status(200).send({ status: 200, message: 'done' })
-    //         }
-    //     });
-    // } catch (error) {
-    //     console.log(error);
-    // }
-})
+    try {
+      // Fetch user's location using the Geolocation API
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const url = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+        console.log(latitude, longitude);
+  
+        // Insert the location into MongoDB
+        const newLoc = new Location({ latitude, longitude, url, status: 'yes' });
+        await newLoc.save();
+        console.log('Location saved successfully!');
+        res.status(200).send({ status: 200, message: 'done' });
+      }, (error) => {
+        console.error(error);
+        res.status(500).send({ status: 500, message: 'Failed to fetch user location' });
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ status: 500, message: 'Internal server error' });
+    }
+  });
+  
 
 router.get('/hello', async (req, res) => {
  console.log('hello');
