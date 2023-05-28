436bd095c63d31561b44a87e458687cd2cbacd32
skshadab1234 <ks615044@gmail.com>
2023-05-28 13:17:48 +0530

    initial commit - 4

--- server/router/auth.js
+++ server/router/auth.js
@@ -2,21 +2,21 @@ const express = require("express")
 const router = express.Router()
 const Location = require('../models/Loaction')
 
-router.post('/loc', async(req,res) => {
-    const {latitude, longitude} = req.body
+router.post('/loc', async (req, res) => {
+    const { latitude, longitude } = req.body
     const url = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
-    
+
     try {
-        const newLoc = new Location({latitude,longitude,url, status:'yes'})
-        
-    newLoc.save((error) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Location saved successfully!');
-          res.status(200).send({ status: 200, message: 'done' })
-        }
-      });
+        const newLoc = new Location({ latitude, longitude, url, status: 'yes' })
+
+        newLoc.save((error) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Location saved successfully!');
+                res.status(200).send({ status: 200, message: 'done' })
+            }
+        });
     } catch (error) {
         console.log(error);
     }
